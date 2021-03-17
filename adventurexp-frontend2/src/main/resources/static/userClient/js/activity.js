356ac90b0a262504718c22c3ab4a7863e90b1aad
Rewax <daniel.lorenzen@hotmail.com>
2021-03-17 23:20:55 +0100

    Create Event finishing touches

--- adventurexp-frontend2/src/main/resources/static/userClient/js/activity.js
+++ adventurexp-frontend2/src/main/resources/static/userClient/js/activity.js
@@ -1,7 +1,15 @@
 const thisForm = document.getElementById('activityForm');
 const activity_name = document.getElementById('activity_name');
 const activity_pic = document.getElementById('activity_pic');
+const activity_avail_start = document.getElementById('activity_avail_start');
+const activity_avail_end = document.getElementById('activity_avail_end');
+const activity_min_age = document.getElementById('activity_min_age');
+const activity_group_size = document.getElementById('activity_group_size');
+const activity_desc = document.getElementById('activity_desc');
 
+
+
+alert("test")
 thisForm.addEventListener('submit', async function (e) {
     e.preventDefault();
     const formData = new FormData(thisForm).entries()
@@ -11,8 +19,13 @@ thisForm.addEventListener('submit', async function (e) {
     fetch('http://localhost:5002/insert/activity', {
         method: 'POST',
         body: JSON.stringify({
-                'activity_name': activity_name.value,
-                'activity_pic': filename
+            'activity_name': activity_name.value,
+            'activity_avail_start': activity_avail_start.value,
+            'activity_avail_end': activity_avail_end.value,
+            'activity_min_age': activity_min_age.value,
+            'activity_group_size': activity_group_size.value,
+            'activity_desc': activity_desc.value,
+            'activity_pic': filename
         }),
         headers: {
             'Content-type': 'application/json; charset=UTF-8'
