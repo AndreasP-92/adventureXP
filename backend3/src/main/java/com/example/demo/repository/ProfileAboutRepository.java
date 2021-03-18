301c231dbc66468daeeaa99056ecf658661681b6
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2021-03-18 21:33:17 +0100

    DELETED FILES

--- backend3/src/main/java/com/example/demo/repository/ProfileAboutRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Profile_about;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileAboutRepository extends JpaRepository<Profile_about, Long> {
-Profile_about findAllByUser_id(int user_mail);
-}
--- code/src/main/java/adventurexp/demo/repository/UserTicketRepository.java
+++
@@ -1,7 +0,0 @@
-package adventurexp.demo.repository;
-
-import adventurexp.demo.model.UserTicket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTicketRepository extends JpaRepository<UserTicket, Integer> {
-}
