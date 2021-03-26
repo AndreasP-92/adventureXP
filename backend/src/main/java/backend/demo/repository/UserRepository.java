20bc5b57ea6c9c54b3a599e2842198c08b0f2041
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2021-03-26 15:41:25 +0100

    Delete backend directory

--- backend/src/main/java/backend/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.demo.repository;
-
-import backend.demo.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-}
